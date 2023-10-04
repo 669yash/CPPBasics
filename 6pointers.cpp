9e55645a3beec28bc3d26fa04d09777145fb699c
Yash Anil Mali <139006542+669yash@users.noreply.github.com>
2023-10-04 15:37:13 +0530

    6pointers.cpp

--- 6pointers.cpp
+++ 6pointers.cpp
@@ -5,22 +5,37 @@ int main(){
 	
 	//pointer: it is a datatype which stores addressess of another datatype
 	
-	int a;
-	cout<<"Enter value of a"<<endl;
+	int a, x;
+	cout<<"Enter value of a: "<<endl;
 	cin>>a;
+	cout<<"Enter value of x: "<<endl;
+	cin>>x;
 	
 	int* b=&a;  
 	
-	cout<<b<<endl; 
+	cout<<"b: "<<b<<endl; 
 	//&-->(address of) operator
-	cout<<&a<<endl; 
+	cout<<"&b: "<<&a<<endl; 
 	//*-->(value at) dereference operator
-	cout<<*b<<endl;
+	cout<<"*b: "<<*b<<endl;
 	
 	//pointer to pointer 
 	int** c=&b;
 	
-	cout<<c<<endl;
+	cout<<"c: "<<c<<endl;
+	
+	//References
+	int &ref=a;
+	ref=80;
+	
+	cout<<"Ref: "<<ref<<endl;
+	cout<<"&a: "<<&a<<endl;
+	cout<<"&ref: "<<&ref<<endl;
+	ref=x;
+	
+	cout<<"Ref: "<<ref<<endl;
+	cout<<"a: "<<a<<endl;
+	cout<<"x: "<<x<<endl;
 	
 	return 0;
 }
